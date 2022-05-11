4a3879fbb392e94187df7b94bdabd653a966f174
jonghyun <jkwhd12@gmail.com>
2022-05-11 11:19:07 +0900

    0511 11:20

--- JavaProgramming/src/Day02/_03_advancedFor.java
+++ JavaProgramming/src/Day02/_03_advancedFor.java
@@ -15,9 +15,11 @@ public class _03_advancedFor {
 		
 		System.out.println("점수의 총합은 : "+ sum1);
 		
+		//scores 배열의 요소 개수만큼 반복
+		//scores 배열의 요소 하나씩 꺼내서 score 변수에 담아서 사용
 		for (int score : scores) { //배열 요소의 갯수만큼 요소를 하나씩 꺼내서 사용하는 방식
 			sum += score;
-		}
+		}// for이 scores 배열 요소만큼 3번 돌거고 score에 담고 sum에 합한 값이 총합이 된다.
 		
 		System.out.println("점수의 총합은 : " + sum);
 	}
